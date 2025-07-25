4870d23a87fb9a6a0e4289b2b60bd367c26f72ba
Kshitiz <vermaksitech7@gmail.com>
2025-07-25 21:03:42 +0530

    Fix: corrected Footer component import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Footer from './components/footer/footer'
+import Footer from './components/footer/Footer'
 import MainRoutes from './routes/MainRoutes'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
